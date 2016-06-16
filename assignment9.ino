35b779ec6fd3eef027b33d0440a19afb1a25e668
Vanikos <nikosvarnavas02@gmail.com>
2016-06-16 11:39:31 +0300

    Update assignment9.ino

--- assignment9.ino
+++ assignment9.ino
@@ -52,16 +52,39 @@ void loop() {
   if (rightDistance == 0 && leftDistance == 0 ) {
     folkracer.setSpeed(0.3);
     folkracer.setAngle(0);
+  } 
+  
+  if (leftDistance == 0 && rightDistance > 0 && rightDistance < 30 ) { // αριστερα 
+    folkracer.setAngle(50);
   }
-
-  if (leftDistance == 0 && rightDistance > 0 && rightDistance < 30 ) {
+  if ( leftDistance > 30 && rightDistance > 0 && rightDistance < 30 ) { // αριστερα
+    folkracer.setAngle(50);
+  }
+  
+  if ( leftDistance > 0 && leftDistance < 30 &&  rightDistance > 30 ) { //δεξια 
+    folkracer.setAngle(50);
+  }
+  
+  if ( leftDistance == 0 && rightDistance == 0 ) { //ευθεια
+    folkracer.setAngle(50);
+  }
+  
+  if (leftDistance == 0 && rightDistance > 30 ) { //ευθεια
     folkracer.setAngle(50);
   }
 
-  if ( leftDistance > 30 && rightDistance > 0 && rightDistance < 30 ) {
+  if (leftDistance > 0 && leftDistance < 30 && rightDistance == 0 ) { //δεξια
     folkracer.setAngle(50);
   }
-
-  if ( leftDistance > 0 && leftDistance < 30 &&  rightDistance < 30 ) {
+  
+  if (leftDistance > 0 && leftDistance < 30 && rightDistance > 0 && rightDistance < 30 ) { //ευθεια
+    folkracer.setAngle(50);
+  }
+  
+  if ( leftDistance > 30 &&  rightDistance == 0 ) { //ευθεια
+    folkracer.setAngle(50);
+  }
+  
+  if ( leftDistance > 30 &&  rightDistance > 30 ) { // ευθεια 
     folkracer.setAngle(50);
   }
